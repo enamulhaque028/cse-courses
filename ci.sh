6dad00d316a2a8676b750a8b70c83936a002414b
Sakib Rahman <sakib.rahman.0000@gmail.com>
2017-10-14 13:06:18 +0600

    [ Done - init ]  CSE345, CSE350, CSE375... Fall 2017(EWU)

---
+++ ci.sh
@@ -0,0 +1,97 @@
+##################################################
+### Script : ci.sh (14-10-2017)                ###
+### CI     : Continious Integration            ###
+### Target : Tool dev			       ###
+###	     Automated Continious Integration  ###
+###	     among local & remote repo	       ###
+##################################################
+
+
+
+
+
+###############################
+### Setting up a repository ###
+###############################
+
+
+
+
+
+############
+# Step: 01 #
+############
+
+echo
+echo 01. Done...Initializing who am I.
+git config --global user.email "sakib.rahman.0000@gmail.com"
+git config --global user.name "Sakib Rahman"
+
+
+############
+# Step: 02 #
+############
+ 
+echo 02. Done...Initializing a new Git repo for this project.
+echo
+git init
+
+
+############
+# Step: 03 #
+############
+
+echo
+echo 03. Done...Saving changes to the repository.
+git add .
+
+
+############
+# Step: 04 #
+############
+
+echo 04. Done...Committing a modified version of a file to the repo.
+echo 
+git commit -m "[ Done - init ]  CSE345, CSE350, CSE375... Fall 2017(EWU)"
+
+
+############
+# Step: 05 #
+############
+
+# List your existing remotes in order to get the name of the remote you want to change.
+echo
+echo 05. Done...checking remote origin.
+echo
+git remote -v
+
+
+############
+# Step: 06 #
+############
+
+# Change your remote's URL from SSH to HTTPS with the git remote set-url command.
+echo
+echo 06. Done...Updating remote URL.
+git remote set-url origin https://github.com/Sakib-Rahman-Bangladesh/cse-courses
+
+
+############
+# Step: 07 #
+############
+
+# Verify that the remote URL has changed.
+echo 07. Done...Verifying remote URL.
+echo
+git remote -v
+
+
+############
+# Step: 08 #
+############
+
+echo
+echo 08. Pushing local codebase to remote repo...Repo-to-repo collaboration: git push
+echo
+git push origin master
+git push --all -f https://github.com/Sakib-Rahman-Bangladesh/cse-courses
