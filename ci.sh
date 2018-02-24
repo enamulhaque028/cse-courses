b8b119b31e83f452d630e4b2cb2e772ba283a125
Sakib Rahman <sakib.rahman.0000@gmail.com>
2018-02-24 15:36:06 +0600

    Released CSE360_2018.Feb23.1.3.0_15/89pages

--- ci.sh
+++ ci.sh
@@ -61,7 +61,7 @@ echo
 ## Delete file.ext
 ## Default: Commit skipped|forgotten
 
-git commit -m "Released CSE360_2018.Feb23.1.2.0_7/73pages"
+git commit -m "Released CSE360_2018.Feb23.1.3.0_15/89pages"
 
 
 ############
