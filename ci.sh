7966244f2dee073ee1c375629187effefe23b82d
Sakib Rahman <sakib.rahman.0000@gmail.com>
2018-03-08 08:53:06 +0600

    CSE498: Init slides

--- ci.sh
+++ ci.sh
@@ -64,7 +64,7 @@ echo
 ## CSE360: IO MODULE ...Change detected, upload
 ## Released CSE360_2018.Feb28.1.5.0_17/107pages
 
-git commit -m "CSE360: Change detected, upload"
+git commit -m "CSE498: Init slides"
 
 ############
 # Step: 05 #
