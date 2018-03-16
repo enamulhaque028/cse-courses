bbd91e0e2bd59519cee596b7917938f55307c135
Sakib Rahman <sakib.rahman.0000@gmail.com>
2018-03-17 01:15:09 +0600

    Upload changes

---
+++ CSE498 - Social and Professional Issues in Computing/communication-media.md
@@ -0,0 +1,4 @@
+Communication media
+- Print media: Newspapers, books, magazines, pamphlets, journals
+- Broadcast media:Television, Radio 
+- Common Carrier: Telephone, Telegraph, Cell phone, Email, and the Postal system
--- ci.sh
+++ ci.sh
@@ -64,7 +64,7 @@ echo
 ## CSE360: IO MODULE ...Change detected, upload
 ## Released CSE360_2018.Feb28.1.5.0_17/107pages
 
-git commit -m "CSE498: Update README.md"
+git commit -m "Upload changes"
 
 ############
 # Step: 05 #
