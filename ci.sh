fefa2c2139242ae45356368327e3b68f49f5b616
Sakib Rahman <sakib.rahman.0000@gmail.com>
2018-03-19 14:36:17 +0600

    CSE411: upload changes

--- ci.sh
+++ ci.sh
@@ -64,7 +64,7 @@ echo
 ## CSE360: IO MODULE ...Change detected, upload
 ## Released CSE360_2018.Feb28.1.5.0_17/107pages
 
-git commit -m "CSE411: presentation"
+git commit -m "CSE411: upload changes"
 
 ############
 # Step: 05 #
