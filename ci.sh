26b6468b3421815a17e0c440f87b8eead8ccad3e
Sakib Rahman <sakib.rahman.0000@gmail.com>
2018-03-20 09:24:00 +0600

    Update README.md

--- ci.sh
+++ ci.sh
@@ -64,7 +64,7 @@ echo
 ## CSE360: IO MODULE ...Change detected, upload
 ## Released CSE360_2018.Feb28.1.5.0_17/107pages
 
-git commit -m "CSE411: upload changes"
+git commit -m "Update README.md"
 
 ############
 # Step: 05 #
